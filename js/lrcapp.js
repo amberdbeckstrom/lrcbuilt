257ab8596982a3611c666c892cf4fcb67f37dbe2
Amber Moss <amberdmoss@gmail.com>
2015-04-05 21:13:04 -0600

    cart changes

--- js/lrcapp.js
+++ js/lrcapp.js
@@ -22,6 +22,15 @@ lrcapp.config([
         .when('/shop', {
             templateUrl: 'templates/shop.html'
         })
+        .when('/cart', {
+            templateUrl: 'templates/cart.html'
+        })
+        .when('/shirt', {
+            templateUrl: 'templates/shirt.html'
+        })
+        .when('/thankyou', {
+            templateUrl: 'templates/thankyou.html'
+        })
         .otherwise({
             redirectTo: '/'
         });
@@ -29,11 +38,44 @@ lrcapp.config([
 ]);
 
 lrcapp.controller('CartController', ['$scope', function($scope) {
-    $scope.cart = 0;
+    $scope.cart = [];
+    $scope.cart_total = 0;
 
-    $scope.add_to_cart = function () {
-        $scope.cart += 1;
+    /**
+     * Add an item to the cart
+     * @param {string} item
+     */
+    function add_cart_item(item) {
+        var in_cart = false;
+
+        $scope.cart.forEach(function (cart_item) {
+            if (cart_item.name == item) {
+                in_cart = true;
+                cart_item.amount += 1;
+            }
+        });
+
+        if (!in_cart) {
+            $scope.cart.push({
+                name: item,
+                amount: 1
+            });
+        }
+    }
+
+    $scope.add_to_cart = function (item) {
+        add_cart_item(item);
+        $scope.cart_total += 1
         console.log($scope.cart);
+    }
+
+    $scope.update_item = function (item) {
+        console.log(item)
+        if (item.amount == 0) {
+            $scope.cart = $scope.cart.filter(function (cart_item) {
+                return cart_item.name != item.name
+            });
+        }
     }
 
     console.log($scope.cart);
